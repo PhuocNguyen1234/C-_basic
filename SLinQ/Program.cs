98d8d9bb83a5df87df15777c214b0f90fc83ba1c
Nguyen Phuoc <163439962+PhuocNguyen1234@users.noreply.github.com>
2025-08-27 20:20:59 +0700

    SLinQ

---
+++ SLinQ/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace SLinQ
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var dataSource = DanhSach();
+            Print(dataSource);
+            var query = from data in dataSource
+                        where GreaterThan0(data)
+                        select data;
+            //var query = dataSource.Where(data => GreaterThan0(data));
+            Print(query);
+            Console.WriteLine("Tong cac so lon hon 0");
+            Console.WriteLine(query.Sum());
+            Console.WriteLine($"Co bao nhieu so lon hon 0: {query.Count()}");
+            Console.ReadLine();
+
+
+        }
+        static Boolean GreaterThan0(int n)
+        {
+            return n > 0;
+        }
+        static IEnumerable<int> DanhSach() 
+        {
+            var ds = new[] { 10, 9, 19, 20, -2, 14 };
+            return ds;
+        }
+        static void Print(IEnumerable<int> value)
+        {
+            Console.WriteLine("--------------");
+            foreach(var i in value)
+            {
+                Console.WriteLine(" " + i);
+            }
+        }
+    }
+}
